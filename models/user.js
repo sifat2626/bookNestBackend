4a90142f58edc2c11e1a7501f168cb84b1bb53d5
sifat2626 <ashrafulsifat26@gmail.com>
2023-07-25 09:17:38 +0600

    register

--- models/user.js
+++ models/user.js
@@ -1,36 +1,32 @@
-const mongoose =require("mongoose");
-const { Schema } = mongoose;
+const mongoose = require("mongoose");
 
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      trim: true,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
-      trim: true,    
       required: true,
       unique: true,
+      trim: true,
     },
     password: {
       type: String,
       required: true,
-      min: 6,
-      max: 64,
-    },
-    address: {
-      type: String,
       trim: true,
     },
     role: {
       type: Number,
       default: 0,
     },
+
   },
-  { timestamps: true,versionKey:false }
+  { versionKey: false, timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -7,29 +7,10 @@ const { requireSignin, isAdmin } =require("../middlewares/auth.js");
 // controllers
 const {
   register,
-  login,
-  secret,
-  updateProfile,
-  getOrders,
-  allOrders,
+
 } =require("../controllers/auth.js");
 
 router.post("/register", register);
-router.post("/login", login);
-router.get("/auth-check", requireSignin, (req, res) => {
-  res.json({ ok: true });
-});
-router.get("/admin-check", requireSignin, isAdmin, (req, res) => {
-  res.json({ ok: true });
-});
 
-router.put("/profile", requireSignin, updateProfile);
-
-// testing
-router.get("/secret", requireSignin, isAdmin, secret);
-
-// orders
-router.get("/orders", requireSignin, getOrders);
-router.get("/all-orders", requireSignin, isAdmin, allOrders);
 
 module.exports= router;
