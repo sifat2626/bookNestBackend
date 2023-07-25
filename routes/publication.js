f116753fd5f4372ba9898fa167aa4c56dab4d1da
sifat2626 <ashrafulsifat26@gmail.com>
2023-07-25 10:37:06 +0600

    Update publication.js

--- routes/publication.js
+++ routes/publication.js
@@ -1,10 +1,29 @@
-const express = require("express");
-
+const express = require('express');
 const router = express.Router();
+const formidable =require("express-formidable");
 
-// middlewares
-const { requireSignin, isAdmin } = require("../middlewares/auth.js");
-// controllers
+const {
+	getAllPublications,
+	getPublicationById,
+	createPublication,
+	updatePublication,
+	deletePublication
+} = require('../controllers/publication');
+const {requireSignin, isAdmin} = require("../middlewares/auth");
 
+// GET all publications
+router.get('/publications', getAllPublications);
+
+// GET a specific publication by ID
+router.get('/publications/:id', getPublicationById);
+
+// CREATE a new publication
+router.post('/publications',requireSignin,isAdmin,formidable(), createPublication);
+
+// UPDATE an existing publication
+router.put('/publications/:id', updatePublication);
+
+// DELETE a publication
+router.delete('/publications/:id', deletePublication);
 
 module.exports = router;
