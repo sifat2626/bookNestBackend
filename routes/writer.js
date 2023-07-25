dce952338a461ce4820ce6d05ebe2a10db2a7d55
sifat2626 <ashrafulsifat26@gmail.com>
2023-07-25 11:02:38 +0600

    Update writer.js

--- routes/writer.js
+++ routes/writer.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const formidable =require("express-formidable");
 
 
+const {createWriter} = require('../controllers/writer');
+const {requireSignin, isAdmin} = require("../middlewares/auth");
+
+router.post('/writers',requireSignin,isAdmin,formidable(),createWriter)
 module.exports = router;
